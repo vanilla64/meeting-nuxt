e7076680c3326687ec9501e97e64c33e3f713483
Sergey <vanillaen@yandex.ru>
2021-08-25 11:04:39 +0400

    change nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: 'meeting-nuxt'
+    base: '/meeting-nuxt/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
